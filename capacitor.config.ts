3d6143a74a9bd2edd8e0a028b101b9381cdbe8ea
Edgar Ruiz Dorador <edgar.ruizdrd@uanl.edu.mx>
2021-06-09 23:33:01 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'todo_list_1990126',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
